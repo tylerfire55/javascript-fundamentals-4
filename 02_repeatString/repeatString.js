77e91faf956dfd7bd37c04f5b8ecaece3aae691d
tylerfire55 <tylergolpe@yahoo.com>
2023-08-09 18:41:27 -0400

    Completed the first 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,16 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+        if (num < 0) {
+            return "ERROR";
+        } else if (num == 0) {
+            return "";
+        } else {
+            for (i = 0; i < num; i++) {
+                return string.repeat(num);
+            }
+        }
 };
+let string = "";
+let num = 0;
 
 // Do not edit below this line
 module.exports = repeatString;

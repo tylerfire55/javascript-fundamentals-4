5fcbaea10d0e66f001bf7c660a0c28f025ba8b02
tylerfire55 <tylergolpe@yahoo.com>
2023-08-10 01:12:38 -0400

    Completed up to 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...numbers) {
+    for (i = 0; i <= numbers.length; i++) {
+        for (j = 0; j <= array.length; j++) {
+            if (numbers[i] === array[j]) {
+                array.splice(j, 1);
+                break
+            }
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line

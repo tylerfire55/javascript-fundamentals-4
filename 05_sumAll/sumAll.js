73c792a3ab2b28ab7aa95dbb42bc841e880bf90f
tylerfire55 <tylergolpe@yahoo.com>
2023-08-11 00:11:25 -0400

    Finished 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
-
+const sumAll = function(x, y) {
+    let sum = 0;
+    if (x >= 0 && y >= 0 && typeof x == "number" && typeof y == "number") {
+        if (y < x) {
+            for (i = y; i <= x; i++) {
+                sum += i;
+            }
+        } else {
+            for (i = x; i <= y; i++) {
+                sum += i;
+            }
+        }
+    }
+    else {
+        return "ERROR";
+    }
+    
+    return sum;
 };
 
 // Do not edit below this line

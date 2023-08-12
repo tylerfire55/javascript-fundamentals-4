49de2332e9f95401b492eaa0b89c8a65bf1c8e30
tylerfire55 <tylergolpe@yahoo.com>
2023-08-11 21:41:22 -0400

    Finished up to 7, aka p1

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,9 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    if(year % 100 == 0){
+        return ((year % 400 == 0) ? true : false);
+    } else {
+        return ((year % 4 == 0 && year % 100 != 0) ? true : false);
+    }
 };
 
 // Do not edit below this line
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheiht) {
+  let convertC = ((fahrenheiht-32) * 5 / 9 );
+  return Math.round(convertC*10)/10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  let convertF = ((celsius * 9 / 5) + 32);
+  return Math.round(convertF*10)/10;
 };
 
 // Do not edit below this line
